2bbeea99b0fd4d39d8047bf2f8770804a56ca4e7
Dedibeat <162831182+Dedibeat@users.noreply.github.com>
2025-04-08 22:08:14 +0800

    Update ulzii.cpp

--- ulzii.cpp
+++ ulzii.cpp
@@ -2,8 +2,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 using cd = complex<double>;
-const cd i(0, 1); // i - хийсвэр тоо
+const cd i(0, 1); // хийсвэр тоо i
 const double PI = acos(-1);
+
+const int WINDOW_WIDTH = 200;
+const int WINDOW_HEIGHT = 200;
 
 // Хээг зурах цэгүүдийн дараалал буюу зам
 vector<pair<double, double>> path;
@@ -29,7 +32,7 @@ void rotate(cd center, double degree)
 {
     save(pen);
     cd vec = pen - center;
-    for(int i = 1; i < 1000; i++)
+    for (int i = 1; i < 1000; i++)
     {
         double angle = degree * i / 1000;
         cd pt = center + vec * exp(cd(0, angle));
@@ -70,24 +73,131 @@ void find_path()
     save(pen);
 }
 
+// Функц: OpenGL пикселүүдийг уншиж BMP файлд бичих
+// Энэ хувилбараар өгөгдлийг өөрчлөхдөө пикселүүдийг BGR болгож хөрвүүлж хэвлэнэ,
+void saveScreenshotBMP(const char* filename, int width, int height)
+{
+    // BMP файлд мөр бүр 4-байт-ын үржлийг шаарддаг.
+    int rowSize = (3 * width + 3) & (~3);
+    int imageSize = rowSize * height;
+    int fileSize = 14 + 40 + imageSize; // 14: BMP Header, 40: DIB Header
+
+    // BMP файлын толгой (Header)
+    unsigned char bmpFileHeader[14] = {
+        'B', 'M',              // Signature "BM"
+        0, 0, 0, 0,            // File size (to be filled)
+        0, 0,                  // Reserved1
+        0, 0,                  // Reserved2
+        54, 0, 0, 0            // Offset to pixel data (54 bytes)
+    };
+
+    unsigned char bmpInfoHeader[40] = {
+        40, 0, 0, 0,           // DIB header size (40 bytes)
+        0, 0, 0, 0,            // Width (filled later)
+        0, 0, 0, 0,            // Height (filled later)
+        1, 0,                  // Planes (must be 1)
+        24, 0,                 // Bits per pixel (24 for RGB)
+        0, 0, 0, 0,            // Compression (none)
+        0, 0, 0, 0,            // Image size (0 for uncompressed)
+        0, 0, 0, 0,            // X pixels per meter
+        0, 0, 0, 0,            // Y pixels per meter
+        0, 0, 0, 0,            // Total colors (0 = default)
+        0, 0, 0, 0             // Important colors (0 = all)
+    };
+
+    // Fill in file size.
+    bmpFileHeader[ 2] = (unsigned char)(fileSize      );
+    bmpFileHeader[ 3] = (unsigned char)(fileSize >>  8);
+    bmpFileHeader[ 4] = (unsigned char)(fileSize >> 16);
+    bmpFileHeader[ 5] = (unsigned char)(fileSize >> 24);
+
+    // Fill in width and height in bmpInfoHeader.
+    bmpInfoHeader[ 4] = (unsigned char)(width      );
+    bmpInfoHeader[ 5] = (unsigned char)(width >>  8);
+    bmpInfoHeader[ 6] = (unsigned char)(width >> 16);
+    bmpInfoHeader[ 7] = (unsigned char)(width >> 24);
+
+    bmpInfoHeader[ 8] = (unsigned char)(height      );
+    bmpInfoHeader[ 9] = (unsigned char)(height >>  8);
+    bmpInfoHeader[10] = (unsigned char)(height >> 16);
+    bmpInfoHeader[11] = (unsigned char)(height >> 24);
+
+    // Fill in image size in bmpInfoHeader.
+    bmpInfoHeader[20] = (unsigned char)(imageSize      );
+    bmpInfoHeader[21] = (unsigned char)(imageSize >>  8);
+    bmpInfoHeader[22] = (unsigned char)(imageSize >> 16);
+    bmpInfoHeader[23] = (unsigned char)(imageSize >> 24);
+
+    // Allocate memory for pixel data (3 channels: RGB)
+    unsigned char* pixels = new unsigned char[width * height * 3];
+    glPixelStorei(GL_PACK_ALIGNMENT, 1);
+
+    // Ensure all drawing commands have completed
+    glFinish();
+    // Read the pixels from the back buffer (image is already bottom-up as needed)
+    glReadBuffer(GL_BACK);
+    glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+
+    // Convert pixel data to BMP format (swap RGB to BGR, but do not flip vertically)
+    unsigned char* bmpData = new unsigned char[imageSize];
+    memset(bmpData, 0, imageSize);
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            int srcIndex = (y * width + x) * 3;
+            int dstIndex = (y * rowSize) + (x * 3);
+            bmpData[dstIndex + 0] = pixels[srcIndex + 2]; // Blue channel
+            bmpData[dstIndex + 1] = pixels[srcIndex + 1]; // Green channel
+            bmpData[dstIndex + 2] = pixels[srcIndex + 0]; // Red channel
+        }
+    }
+
+    // Write the BMP file using standard C++ libraries
+    ofstream out(filename, ios::binary);
+    if (out)
+    {
+        out.write(reinterpret_cast<char*>(bmpFileHeader), sizeof(bmpFileHeader));
+        out.write(reinterpret_cast<char*>(bmpInfoHeader), sizeof(bmpInfoHeader));
+        out.write(reinterpret_cast<char*>(bmpData), imageSize);
+        out.close();
+        cout << "Saved screenshot as " << filename << "\n";
+    }
+    else
+    {
+        cerr << "Error: could not write " << filename << "\n";
+    }
+
+    delete[] pixels;
+    delete[] bmpData;
+}
+
 // Зургийг зурах буюу дэлгэцэнд харуулах функц
 void display() 
 {
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
     gluOrtho2D(-400, 400, -400, 400);
     
     // 135 градусаар нар буруу эргүүлнэ
-    glRotatef(135, 0, 0, 1);  
-
+    glRotatef(-3, 0, 0, 1);
+    
     glBegin(GL_LINE_STRIP);
     glColor3f(0.0f, 0.0f, 0.0f);
-    for (auto [x, y] : path) 
+    for (auto [x, y] : path)
         glVertex2d(x, y);
     glEnd();
-
-
+    
+    // Capture the screenshot from the back buffer BEFORE swapping.
+    static bool screenshotSaved = false;
+    if (!screenshotSaved)
+    {
+        glFinish();
+        saveScreenshotBMP("dul.bmp", WINDOW_WIDTH, WINDOW_HEIGHT);
+        screenshotSaved = true;
+    }
+    
     glutSwapBuffers();
 }
 
@@ -95,7 +205,7 @@ int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(800, 800);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
     glutCreateWindow("УЛЗИЙ ХЭЭ");
 
     glEnable(GL_LINE_SMOOTH);
@@ -106,8 +216,7 @@ int main(int argc, char **argv)
 
     glutDisplayFunc(display);
     glClearColor(0, 0, 0, 1);
-
     
     glutMainLoop();
-    return 8 >> 13;
+    return 0;
 }
